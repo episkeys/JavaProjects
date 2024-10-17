6e3b9ce07b1ff1e5b58ec67ed3defd33512643cc
Episkeys <86928284+episkeys@users.noreply.github.com>
2024-10-17 09:42:23 +0300

    updated

---
+++ Loops/findMaxMin.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class findMaxMin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int lastNumber = 0;
+        int secondLastNumber = 0;
+        int count = 0;
+
+        while (true) {
+            System.out.println("Enter number: ");
+            int n = sc.nextInt();
+            if (n < 0) {
+                break;
+            }
+            if (count >= 1) {
+                secondLastNumber = lastNumber;
+            }
+            lastNumber = n;
+            count++;
+
+            if(count >= 2){
+                if (lastNumber < secondLastNumber) {
+                    System.out.println("Smaller than previous number\n");
+                } else {
+                    System.out.println("Greater than previous number\n");
+                }
+            }
+
+        }
+
+
+
+
+
+    }
+
+
+}
